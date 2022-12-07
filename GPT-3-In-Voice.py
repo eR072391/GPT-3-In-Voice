52fe24ef52540026ef39e7af541e8e1a08ea9d4d
er072391 <76501872+eR072391@users.noreply.github.com>
2022-12-07 21:35:13 +0900

    Create GPT-3-In-Voice.py

---
+++ GPT-3-In-Voice.py
@@ -0,0 +1,52 @@
+import speech_recognition as sr
+import openai
+from gtts import gTTS           #文字を音声に変換
+from playsound import playsound #音声を再生
+
+#GPT-3のAPIキーをここに
+OPENAI_API_KEY = ''
+
+def play_sound(answer):
+    tts = gTTS(text=answer,lang="ja")
+    tts.save('/tmp/voice.mp3')
+    playsound("/tmp/voice.mp3")
+
+def send_openai(question):
+    # GPT-3のAPIキーを設定する
+    openai.api_key = OPENAI_API_KEY
+
+    # GPT-3を使用して文章を生成する
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=question,
+        max_tokens=1024,
+        temperature=0.5,
+    )
+
+    # 生成した文章を表示する
+    print("[*]回答")
+    answer = response["choices"][0]["text"]
+    #play_sound(answer) #回答を音声に変換して再生
+    print(answer)
+    
+    
+def main():
+    while True:
+        # 音声を読み込む
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+
+        try:
+            # 音声を文字列に変換する
+            text = r.recognize_google(audio, language='ja-JP')
+            print("「",text,"」")
+            if "終了" in text:
+                break
+            send_openai(text)
+        except:
+            print("[*]音声入力なし")
+            pass
+    
+if __name__ == '__main__':
+    main()
